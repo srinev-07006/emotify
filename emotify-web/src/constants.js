ce14108d84dd6d7823a996670ab560ba4b5afdfa
Srideep Nair <srideepnair1106@gmail.com>
2026-08-21 13:42:31 +0530

    npm app build updates

--- emotify-web/src/constants.js
+++ emotify-web/src/constants.js
@@ -1,2 +1,2 @@
-const BASE_URL = 'http://localhost:8080'; // replace with your IPv4 from ipconfig
+const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 export default BASE_URL;
